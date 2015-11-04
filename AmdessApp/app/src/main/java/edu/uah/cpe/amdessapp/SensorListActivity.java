29c4b30bdb0ca8082924ca3af017cda74a545447
John Wilkes <jwilkes0011@gmail.com>
2015-11-03 20:48:01 -0600

    Adding scan activity

--- AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/SensorListActivity.java
+++ AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/SensorListActivity.java
@@ -1,15 +1,23 @@
 package edu.uah.cpe.amdessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SensorListActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_list);
+    }
+
+    public void onScanClick(View v)
+    {
+        // launch the scan activity
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
     }
 }
