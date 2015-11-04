538306f3005fe86dd89fc9f31f39d0aced6c7c06
John Wilkes <jwilkes0011@gmail.com>
2015-11-03 20:23:00 -0600

    Initial commit of AMDeSS app

---
+++ AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/SensorListActivity.java
@@ -0,0 +1,15 @@
+package edu.uah.cpe.amdessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_list);
+    }
+}
