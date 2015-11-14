598d59a9e9026afdf2e4dfe2f460f37651ccd00c
John Wilkes <jwilkes0011@gmail.com>
2015-11-14 17:24:23 -0600

    Sending notification on receiving immediate alert

--- AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/BluetoothLeService.java
+++ AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/BluetoothLeService.java
@@ -1,9 +1,12 @@
 package edu.uah.cpe.amdessapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
 import android.os.Binder;
@@ -34,9 +37,7 @@ public class BluetoothLeService extends Service
 
                 setConnectionStatus(address, true);
 
-                Intent intent = new Intent(Constants.ACTION_GATT_CONNECTED);
-                intent.putExtra(Constants.INFO_DEVICE_ADDRESS, address);
-                sendBroadcast(intent);
+                broadcastUpdate(Constants.ACTION_GATT_CONNECTED, address);
             }
             else if (newState == BluetoothProfile.STATE_DISCONNECTED)
             {
@@ -44,9 +45,16 @@ public class BluetoothLeService extends Service
 
                 setConnectionStatus(address, false);
 
-                Intent intent = new Intent(Constants.ACTION_GATT_DISCONNECTED);
-                intent.putExtra(Constants.INFO_DEVICE_ADDRESS, address);
-                sendBroadcast(intent);
+                broadcastUpdate(Constants.ACTION_GATT_DISCONNECTED, address);
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic)
+        {
+            if (characteristic.getUuid().equals(Constants.UUID_IMMEDIATE_ALERT))
+            {
+                onReceiveImmediateAlert(gatt, characteristic);
             }
         }
     }
@@ -60,6 +68,8 @@ public class BluetoothLeService extends Service
         }
     }
 
+    private static final int NOTIFICATION_ID = 123;
+    private final long[] VIBRATE_PATTERN = {0, 400, 100, 400, 100, 400, 100, 400};
     private final BtGattCallback gattCallback = new BtGattCallback();
     private BleBinder bleBinder = new BleBinder();
     private BluetoothGatt bluetoothGatt = null;
@@ -105,5 +115,40 @@ public class BluetoothLeService extends Service
 
         // update the info in the map
         infoMap.put(address, info);
+    }
+
+    private void onReceiveImmediateAlert(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic)
+    {
+        Log.d("onReceiveImmediateAlert", "Received immediate alert");
+
+        String name = gatt.getDevice().getName();
+        String address = gatt.getDevice().getAddress();
+        if (name == null)
+        {
+            name = address;
+        }
+        String text = String.format("Alarm from %s", name);
+
+        // send broadcast
+        broadcastUpdate(Constants.ACTION_DEVICE_ALARM, address);
+
+        // send notification
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder =
+                new Notification.Builder(this)
+                    .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                    .setContentTitle("Alarm!")
+                    .setContentText(text)
+                    .setVibrate(VIBRATE_PATTERN);
+        Notification notification = builder.build();
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void broadcastUpdate(String action, String address)
+    {
+        Intent intent = new Intent(action);
+        intent.putExtra(Constants.INFO_DEVICE_ADDRESS, address);
+        sendBroadcast(intent);
     }
 }
--- AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/Constants.java
+++ AmdessApp/app/src/main/java/edu/uah/cpe/amdessapp/Constants.java
@@ -2,8 +2,13 @@ package edu.uah.cpe.amdessapp;
 
 import android.graphics.Color;
 
+import java.util.UUID;
+
 public class Constants
 {
+    public static final String ACTION_DEVICE_ALARM =
+            "edu.uah.cpe.amdessapp.ACTION_DEVICE_ALARM";
+
     public static final String ACTION_GATT_CONNECTED =
             "edu.uah.cpe.amdessapp.ACTION_GATT_CONNECTED";
 
@@ -12,6 +17,13 @@ public class Constants
 
     public final static String INFO_DEVICE_ADDRESS =
             "edu.uah.cpe.amdessapp.INFO_DEVICE_ADDRESS";
+
+    // ------ UUIDs ------
+    public final static UUID UUID_GENERIC_ACCESS = UUID.fromString("00001800-0000-1000-8000-00805f9b34fb");
+    public final static UUID UUID_GENERIC_ATTRIBUTE = UUID.fromString("00001801-0000-1000-8000-00805f9b34fb");
+    public final static UUID UUID_IMMEDIATE_ALERT = UUID.fromString("00001802-0000-1000-8000-00805f9b34fb");
+
+    // ------ Colors ------
 
     public final static int CONNECTED_COLOR = Color.rgb(0, 150, 50);
     public final static int DISCONNECTED_COLOR = Color.rgb(200, 0, 0);
